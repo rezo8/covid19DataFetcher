aea521749ea6e6f52c26bfab6d5f3d11d8e2a2ed
RibhiEl-Zaru <ribhielzaru@gmail.com>
2020-04-08 19:13:57 -0400

    Take one at computing rate of transmission from data. Doesn't seem to work.

--- testScripts.js
+++ testScripts.js
@@ -2,7 +2,48 @@ const { ApiExecutor } = require('./workers/apiExecutor')
 const { CurveModeler } = require('./workers/curveModeler')
 const { CountryDataAggregator} = require('./workers/CountryDataAggregator')
 const apiExecutor = new ApiExecutor('covid-193.p.rapidapi.com', 'a105fbc46emsh9211cb134c839b1p1d1031jsn8f45ca70b43d' )
-const curveModeler = new CurveModeler(apiExecutor)
+
+
+const curveModeler = new CurveModeler([
+                                          0.99,
+                                          0.9702,
+                                          0.916839,
+                                          0.780163976592,
+                                          0.47735465174115765,
+                                          0.04559549641903382,
+                                          0.025532119296638386,
+                                          0.008768554102548759,
+                                          0.0002995205360159757,
+                                          0.00007169442381589448,
+                                          0.000029671067281748675
+                                        ],
+                                        [
+                                          0.01,
+                                          0.0275,
+                                          0.074536,
+                                          0.194067743408,
+                                          0.45224148727500235,
+                                          0.7799851005238756,
+                                          0.6717161431190564,
+                                          0.4829207568024862,
+                                          0.38031801630444717,
+                                          0.2930726986666244,
+                                          0.22570800132983493
+                                        ],
+                                        [
+                                          0,
+                                          0.0023,
+                                           0.008625,
+                                          0.02576828,
+                                          0.07040386098384001,
+                                          0.17441940305709056,
+                                          0.353815976177582,
+                                          0.5083106890949649,
+                                          0.6193824631595367,
+                                          0.7068556069095595,
+                                          0.7742623276028832
+                                        ]
+)
 /*
 apiExecutor.getCountries(function(body){ console.log('there are ' + body.results + ' countries available to look at') })
 
@@ -32,5 +73,7 @@ apiExecutor.getStatsForToday(function(data){
 })
 */
 
-curveModeler.generateInfoGraph(10)
+console.log(curveModeler.rateOfRecovery)
+console.log(curveModeler.rateOfTransmission)
+console.log(curveModeler.generateInfoGraph(10))
 
--- workers/curveModeler.js
+++ workers/curveModeler.js
@@ -2,15 +2,58 @@
 
 class CurveModeler {
 
-    constructor(apiExecutor){
-        this.apiExecutor = apiExecutor
+    constructor(susceptibleArray, infectedArray, recoveryArray){
         this.rateOfRecovery = 0.23
         this.rateOfTransmission = 2.0
+
+        this.computeConstantsWithData(susceptibleArray, infectedArray, recoveryArray)
         this.sStart = .99
         this.iStart = .01
         this.rStart = 0
         this.maxTime = 40 // number of days we compute this for.
     }
+
+    computeConstantsWithData(susceptibleArray, infectedArray, recoveryArray){
+        this.rateOfTransmission = this.computeRateOfTransmissionWithData(susceptibleArray, infectedArray)
+        this.rateOfRecovery = this.computeRateOfRecoveryWithData(susceptibleArray, infectedArray)
+
+
+
+    }
+
+    computeRateOfTransmissionWithData(susceptibleArray, infectedArray){
+
+        let k = Math.min(susceptibleArray.length, infectedArray.length)
+
+
+        let curr = 1
+        let summation = 0
+        while(curr < k - 1){
+            summation += ((susceptibleArray[curr] - susceptibleArray[curr- 1])/(susceptibleArray[curr] * infectedArray[curr]))
+            curr++
+        }
+
+        return summation/k
+    }
+
+
+    computeRateOfRecoveryWithData(recoveryArray, infectedArray){
+
+
+        let k = Math.min(recoveryArray.length, infectedArray.length)
+
+        let curr = 1
+        let avgRate = 0
+        while(curr < k - 1){
+            let rDelta = recoveryArray[curr]  - recoveryArray[curr - 1]
+            let currRate = rDelta/(infectedArray[curr])
+            avgRate = avgRate + currRate
+            curr ++
+        }
+
+        return avgRate/k
+    }
+
 
     generateInfoGraph(n){
 
@@ -55,3 +98,102 @@ class CurveModeler {
 module.exports = {
     CurveModeler: CurveModeler
 }
+
+/*
+
+
+class CurveModeler {
+
+    constructor(susceptibleArray, infectedArray, recoveryArray){
+        this.rateOfRecovery = 0.23
+        this.rateOfTransmission = 2.0
+
+        this.computeConstantsWithData(susceptibleArray, infectedArray, recoveryArray)
+        this.sStart = .99
+        this.iStart = .01
+        this.rStart = 0
+        this.maxTime = 40 // number of days we compute this for.
+    }
+
+    computeConstantsWithData(susceptibleArray, infectedArray, recoveryArray){
+        this.rateOfTransmission = this.computeRateOfTransmissionWithData(susceptibleArray, infectedArray)
+        this.rateOfRecovery = this.computeRateOfRecoveryWithData(susceptibleArray, infectedArray)
+        this.sStart = susceptibleArray[0]
+        this.iStart = infectedArray[0]
+        this.rStart = recoveryArray[0]
+
+    }
+
+    computeRateOfTransmissionWithData(susceptibleArray, infectedArray){
+        let k = Math.min(susceptibleArray.length, infectedArray.length)
+        let curr = 1
+        let summation = 0
+        while(curr < k - 1){
+            summation += susceptibleArray[curr] - infectedArray[curr]
+            curr ++
+        }
+
+        return (susceptibleArray[k-1] - susceptibleArray[0])/summation
+    }
+
+
+    computeRateOfRecoveryWithData(recoveryArray, infectedArray){
+
+
+        let k = Math.min(recoveryArray.length, infectedArray.length)
+
+        let curr = 1
+        let summation = 0
+        while(curr < k - 1){
+            summation += infectedArray[curr]
+            curr ++
+        }
+
+        return recoveryArray[k - 1]/summation
+    }
+
+
+    generateInfoGraph(n){
+
+        var start = 0
+        var Ik = this.iStart
+        var Sk = this.sStart
+        var Rk = this.rStart
+        var infoGraph = {'susceptible' : [Sk], 'infected': [Ik], 'recovered' : [Rk]}
+        var res
+        while(start < n){
+            res = this.performInfoComputation(Sk, Ik, Rk)
+            Sk = res[0]
+            Ik = res[1]
+            Rk = res[2]
+            infoGraph['susceptible'].push(Sk)
+            infoGraph['infected'].push(Ik)
+            infoGraph['recovered'].push(Rk)
+            start ++
+        }
+        return infoGraph
+    }
+
+    performInfoComputation(Sk, Ik, Rk){
+
+        return [this.computeSkPlusOne(Sk, Ik), this.computeIkPlusOne(Sk, Ik), this.computeRkPlusOne(Rk, Ik)]
+    }
+
+
+    computeIkPlusOne(Sk, Ik){
+        return Ik + this.rateOfTransmission * Sk * Ik - this.rateOfRecovery * Ik
+    }
+
+    computeSkPlusOne(Sk, Ik){
+        return Sk - this.rateOfTransmission * Sk * Ik
+    }
+
+    computeRkPlusOne(Rk, Ik){
+        return Rk + this.rateOfRecovery * Ik
+    }
+}
+
+module.exports = {
+    CurveModeler: CurveModeler
+}
+*/
